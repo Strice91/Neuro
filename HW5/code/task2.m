6bf937446c1b75950d0f33648cfd80dc8878c89b
Strice91 <r.strice@gmail.com>
2015-05-18 20:18:50 +0200

    task2 plots fertig

--- HW5/code/task2.m
+++ HW5/code/task2.m
@@ -1,3 +1,5 @@
+close all;
+
 dt = 0.025;
 dur = 100;
 
@@ -16,8 +18,26 @@ active_compart = 20;
 iStim = pulseGen(dur,dt,pulseWidth,pulseDelay,amps,comparts,active_compart);
 [V,t,i_ion,gate] = compartNeuron(V0,t0,dt,dur,iStim,T,comparts);
 
+figure;
+imagesc(V);
+title('Stimulus bei Compartment 20')
+ylabel('Compartment NR.');
+xlabel('t in ms');
+set(gca, 'XTick', 1:500:4001, 'XTickLabel', 0:12.5:100);
+c = colorbar;
+ylabel(c, 'V in mV');
+
 
 %% b)
 active_compart = 80;
 iStim = pulseGen(dur,dt,pulseWidth,pulseDelay,amps,comparts,active_compart);
 [V,t,i_ion,gate] = compartNeuron(V0,t0,dt,dur,iStim,T,comparts);
+
+figure;
+imagesc(V);
+title('Stimulus bei Compartment 80')
+ylabel('Compartment NR.');
+xlabel('t in ms');
+set(gca, 'XTick', 1:500:4001, 'XTickLabel', 0:12.5:100);
+c = colorbar;
+ylabel(c, 'V in mV');
